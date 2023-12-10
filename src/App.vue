3c3089a6d1db3eeb561612d1bed2597c175d5aa6
Bilal Arshad <bilalarshad6780@gmail.com>
2023-12-10 12:45:45 +0500

    My first figma design replica

---
+++ src/App.vue
@@ -0,0 +1,209 @@
+<template>
+  <nav class="w-full relative h-[104px] bg-black flex items-center">
+    <ul class="flex text-white leading-[20px] md:gap-10 md:ml-28 sm:ml-4 sm:gap-8 xs:ml-2 xs:gap-4">
+      <li class="flex">Marketplace</li>
+      <li class="flex">Artists</li>
+      <li class="flex">Community</li>
+      <li class="flex">Collections</li>
+    </ul>
+    <button
+      class="text-white px-4 py-2 bg-yellow-400 rounded-lg ml-auto w-[143px] h-[48px] md:mr-16 sm:mr-4 xs:mr-4">Contact</button>
+  </nav>
+
+  <div class="relative bg-black w-full h-[800px]">
+    <div class=" bg-black bottom-20">
+      <h1 class="absolute text-5xl font-semibold text-white top-32 left-32">
+        Discover and <br>
+        Collect The Best <br>
+        NFTs <span style='color: yellow;'>Digital Art.</span>
+      </h1>
+      <p class="text-white absolute top-80 text-sm ml-32">Get started with the easiest and most secure platform to buy and
+        trade <br> digital ART and NFT’s. Start exploring the world of digital art and NFTs <br> today and take control of
+        your digital assets with confidence!</p>
+      <button class=" absolute h-[52px] w-[178px] rounded-lg bg-yellow-400 text-white ml-32"
+        style="margin-top: 27rem;">Explore Now</button>
+      <h3 class="text-white absolute text-md mx-96" style="margin-top: 28rem;">Learn More</h3>
+      <h1 class=" absolute text-5xl bottom-40 left-28 font-semibold text-yellow-400">8.9<span
+          style="color: white;">K</span></h1>
+      <div class="h-[90px] left-60 bottom-28 border-l border-yellow-400 absolute"></div>
+      <h3 class="absolute text-white text-sm bottom-28 left-28">At work</h3>
+      <h1 class=" absolute text-5xl bottom-40 left-80 font-semibold text-yellow-400">65<span
+          style="color: white;">K</span></h1>
+      <div class="h-[90px] bottom-28 border-l border-yellow-400 absolute" style="margin-left: 27rem;"></div>
+      <h3 class="absolute text-white text-sm bottom-28 left-80">Artist</h3>
+      <h1 class=" absolute text-5xl bottom-40 font-semibold text-yellow-400" style="margin-left: 32rem;">87<span
+          style="color: white;">K</span></h1>
+      <h3 class="absolute text-white text-sm bottom-28" style="margin-left: 32rem;">Collection</h3>
+    </div>
+
+    <div class="float-right relative" style="height: 53rem;">
+      <img src="./images/Group 3.png" class="relative p-24 mr-20 mb-20 z-1" />
+      <div
+        class="grid grid-cols-2 absolute h-[169px] w-[304px] bg-blue-950 right-28 bottom-28 rounded-2xl justify-between"
+        style="margin-top: 32rem;">
+        <div class="float-right mr-10 items-center relative top-5 left-6">
+          <h3 class="text-yellow-400">Ends in</h3>
+          <h3 class="text-white font-bold mt-1">05:45:47</h3>
+        </div>
+        <div class="float-left relative top-5 left-10">
+          <h3 class="text-yellow-400">Current bid</h3>
+          <h3 class="text-white font-bold mt-1">0.24ETH</h3>
+        </div>
+        <button
+          class="h-12 w-[254px] border border-solid rounded-lg border-yellow-400 top-3 left-6 relative font-bold text-yellow-400">Place
+          a Bid</button>
+      </div>
+    </div>
+  </div>
+
+  <div class="relative w-full bg-black h-[900px]">
+    <div class="relative bg-black">
+      <h1 class="text-white absolute text-5xl mt-24 ml-16"><span style="color: yellow;">Amazing</span> and Super <br>
+        Unique Art of This<span style="color: yellow;"> Week</span> </h1>
+      <button class="absolute h-[52px] w-[134px] text-white bg-yellow-400 rounded-lg mt-36 right-32">See All</button>
+    </div>
+    <div class="flex justify-between items-center w-[100%] gap-8 ">
+      <div class="basis-1/3">
+        <div class="h-[498px] w-[410px] bg-blue-950 rounded-3xl mt-72 ml-16">
+          <img src="./images/Rectangle 9669.png" class="absolute h-[340px] w-[370px] rounded-2xl ml-5 mt-5" />
+          <h2 class="text-white absolute mt-96 text-lg font-semibold ml-5">Cyberpunk Cocomo</h2>
+          <p class="text-sm text-white absolute font-semibold" style="margin-left: 21rem; margin-top: 24rem;">490ETH</p>
+          <p class="text-xs absolute text-gray-400 ml-5" style="margin-top: 27rem;">Ending in</p>
+          <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 512 512" class="absolute ml-5" style="margin-top: 28rem;"><path fill="#e6e7eb" d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"/></svg>
+          <p class="text-sm absolute font-bold text-white ml-12" style="margin-top: 28rem;">03:24:58</p>
+          <button class="border-yellow-400 border text-yellow-400 bg-none h-[48px] w-[168px] outline-yellow-400 absolute ml-56 rounded-lg" style="margin-top: 27rem;">Place A Bid</button>
+        </div>
+      </div>
+      <div class="basis-1/3">
+        <div class="h-[498px] w-[410px] bg-blue-950 rounded-3xl mt-72 ml-8">
+          <img src="./images/Rectangle 9668.png" class="absolute h-[340px] w-[370px] rounded-2xl ml-5 mt-5" />
+          <h2 class="text-white absolute mt-96 text-lg font-semibold ml-5">Charge, Qi tiao yu</h2>
+          <p class="text-sm text-white absolute font-semibold" style="margin-left: 21rem; margin-top: 24rem;">490ETH</p>
+          <p class="text-xs absolute text-gray-400 ml-5" style="margin-top: 27rem;">Ending in</p>
+          <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 512 512" class="absolute ml-5" style="margin-top: 28rem;"><path fill="#e6e7eb" d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"/></svg>
+          <p class="text-sm absolute font-bold text-white ml-12" style="margin-top: 28rem;">03:24:58</p>
+          <button class="border-yellow-400 border text-yellow-400 bg-none h-[48px] w-[168px] outline-yellow-400 absolute ml-56 rounded-lg" style="margin-top: 27rem;">Place A Bid</button>
+        </div>
+      </div>
+      <div class="basis-1/3">
+        <div class="h-[498px] w-[410px] bg-blue-950 rounded-3xl mt-72">
+          <img src="./images/Rectangle 9667.png" class="absolute h-[340px] w-[370px] rounded-2xl ml-5 mt-5" />
+          <h2 class="text-white absolute mt-96 text-lg font-semibold ml-5">Surgeon, Josh Rife</h2>
+          <p class="text-sm text-white absolute font-semibold" style="margin-left: 21rem; margin-top: 24rem;">490ETH</p>
+          <p class="text-xs absolute text-gray-400 ml-5" style="margin-top: 27rem;">Ending in</p>
+          <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 512 512" class="absolute ml-5" style="margin-top: 28rem;"><path fill="#e6e7eb" d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"/></svg>
+          <p class="text-sm absolute font-bold text-white ml-12" style="margin-top: 28rem;">03:24:58</p>
+          <button class="border-yellow-400 border text-yellow-400 bg-none h-[48px] w-[168px] outline-yellow-400 absolute ml-56 rounded-lg" style="margin-top: 27rem;">Place A Bid</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="relative w-full bg-black h-[900px]">
+    <div class="relative bg-black">
+      <h1 class="absolute text-white text-5xl mt-72 ml-16">Create And Sell <br> Your <span style="color: yellow;">Best NFTs</span></h1>
+      <p class="text-white absolute ml-16" style="margin-top: 27rem;">Start exploring the world of digital art and NFTs today <br> and take control of your digital assets with confidence!</p>
+      <button class=" absolute h-[52px] w-[178px] rounded-lg bg-yellow-400 text-white ml-16"
+        style="margin-top: 34rem;">Create Now</button>
+      <h3 class="text-white absolute text-md mx-80" style="margin-top: 35rem;">Learn More</h3>
+    </div>
+
+    <div class="float-right relative">
+      <img src="./images/Rectangle 9671.png" class="mr-24 mt-36" />
+      <img src="./images/Rectangle 9672.png" class="absolute top-80" style="right: 27rem;" />
+    </div>
+  </div>
+
+  <div class="relative w-full bg-black h-[900px]">
+    <div class="relative bg-black">
+      <h1 class="text-white text-5xl ml-32 mt-6 absolute">Popular <span style="color: yellow;"> Artists </span><br> On This Week</h1>
+      <button class="absolute h-[52px] w-[134px] text-white bg-yellow-400 rounded-lg mt-16 right-32">See All</button>
+    </div>
+    <div class="grid grid-rows-2 grid-cols-3 justify-between items-center gap-8 relative top-56">
+      <div class="row-span-1 col-span-1">
+        <img src="./images/Group 6.png" class="w-[300px] h-[311px] rounded-2xl relative left-40"/>
+      </div>
+      <div class="row-span-1 col-span-1">
+        <img src="./images/Rectangle 9677.png" class="w-[530px] h-[311px] rounded-2xl relative right-6"/>
+      </div>
+      <div class="row-span-2 col-span-1">
+        <img src="./images/Rectangle 9674.png" class="w-[410px] h-[652px] rounded-2xl relative right-7"/>
+      </div>
+      <div class="row-span-1 col-span-1">
+        <img src="./images/Group 8.png" class="w-[390px] h-[311px] rounded-2xl relative left-40"/>
+      </div>
+      <div class="row-span-1 col-span-1">
+        <img src="./images/Rectangle 9678.png" class="w-[390px] h-[311px] rounded-2xl left-16 relative"/>
+      </div>
+    </div>
+  </div>
+
+  <div class="relative w-full bg-black h-[900px]">
+    <div class="relative bg-black">
+      <img src="./images/Rectangle 9679.png" class="float-left relative left-32" style="margin-top: 14rem;">
+      <img src="./images/Rectangle 9680.png" class="absolute " style="left: 30rem; top: 21rem;">
+    </div>
+    <div class="float-right">
+      <h1 class="text-white text-5xl relative right-24 top-72">Subscribe And <span style="color: yellow;"> get our </span> <br> <span style="color: yellow;"> Updates </span> Every Week</h1>
+      <p class="text-white top-80 right-24 relative">We have a blog related to NFT so we can share thoughts and <br> routines on our blog which is updated weekly</p>
+      <input type="email" placeholder="     Enter your e-mail" class="w-[516px] h-[60px] rounded-lg border-none bg-blue-950 text-white top-96 right-24 relative"/>
+      <button class="h-[58px] w-[160px] rounded-lg bg-yellow-400 text-white absolute right-24" style="top: 33rem;">Subscribe</button>
+    </div>
+  </div>
+
+  <div class="relative w-full bg-black h-[300px]">
+    <div class="relative bg-black">
+      <footer>
+        <div class="grid grid-cols-5 ml-32 relative">
+          <div>
+            <h4 class="text-white font-bold">Quick Links</h4>
+            <ul class="text-white text-sm">
+              <li class="mt-5">About Us</li>
+              <li>Contact Us</li>
+              <li>Privacy Policy</li>
+              <li>Contribute</li>
+            </ul>
+          </div>
+          <div>
+            <h4 class="text-white font-bold">Explore</h4>
+            <ul class="text-white text-sm">
+              <li class="mt-5">Arts Sign In</li>
+              <li>Collectibles</li>
+              <li>Domain Name</li>
+              <li>Utility</li>
+            </ul>
+          </div>
+          <div>
+            <h4 class="text-white font-bold ">Statistics</h4>
+            <ul class="text-white text-sm">
+              <li class="mt-5">Ranking</li>
+              <li>Collectibles</li>
+              <li>Activity</li>
+            </ul>
+          </div>
+          <div>
+            <h4 class="text-white font-bold">Company</h4>
+            <ul class="text-white text-sm">
+              <li class="mt-5">About Us</li>
+              <li>Career</li>
+              <li>Support</li>
+              <li>Partners</li>
+            </ul>
+          </div>
+          <div>
+            <h4 class="text-white font-bold">Resources</h4>
+            <ul class="text-white text-sm">
+              <li class="mt-5">Help Center</li>
+              <li>Platform status</li>
+              <li>Blog</li>
+            </ul>
+          </div>
+        </div>
+        <div class="border-t border-gray-400 w-3/4 mt-14 ml-44"></div>
+        <p class="text-white text-sm float-left ml-20 mt-20">Copyright 2023 - davixq</p>
+        <p class="text-white text-sm float-right mr-32 mt-20">Privacy Policy</p>
+        <p class="text-white text-sm float-right mr-20 mt-20">Terms & Conditions</p>
+      </footer>
+    </div>
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  screens: {
+
+    'xs': '475px',
+      ...defaultTheme.screens,
+
+    'sm': '640px',
+    // => @media (min-width: 640px) { ... }
+
+    'md': '768px',
+    // => @media (min-width: 768px) { ... }
+
+    'lg': '1024px',
+    // => @media (min-width: 1024px) { ... }
+
+    'xl': '1280px',
+    // => @media (min-width: 1280px) { ... }
+
+    '2xl': '1536px',
+    // => @media (min-width: 1536px) { ... }
+
+  },
+  theme: {
+    screens: {
+      'xs': '475px',
+      ...defaultTheme.screens,
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
